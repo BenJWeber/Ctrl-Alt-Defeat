0869cb2281725e36ed30bafdef58c2796ec8cd7d
bobbleheadhobo <supersoup4@gmail.com>
2022-03-02 21:15:41 -0700

    Prints words from input files using images of letters

--- GUI_attempt/src/entity/Words.java
+++ GUI_attempt/src/entity/Words.java
@@ -2,6 +2,8 @@ package GUI_attempt.src.entity;
 
 import GUI_attempt.src.main.GamePanel;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage; 
@@ -23,7 +25,7 @@ public class Words extends Entity {
         wordsY = 250;
     } //end setDefaultValues
 
-    public void getWordsImage(){ 
+    public void getWordsImage(){
         try{ 
             letter_a = ImageIO.read( getClass().getResourceAsStream( "../../res/words/A.png" ) ); 
             letter_b = ImageIO.read( getClass().getResourceAsStream( "../../res/words/B.png" ) ); 
@@ -51,6 +53,7 @@ public class Words extends Entity {
             letter_x = ImageIO.read( getClass().getResourceAsStream( "../../res/words/X.png" ) ); 
             letter_y = ImageIO.read( getClass().getResourceAsStream( "../../res/words/Y.png" ) ); 
             letter_z = ImageIO.read( getClass().getResourceAsStream( "../../res/words/Z.png" ) ); 
+            space = ImageIO.read( getClass().getResourceAsStream( "../../res/words/space.png" ) );
         } catch( IOException e ){ 
             e.printStackTrace(); 
         } //end catch
@@ -64,7 +67,42 @@ public class Words extends Entity {
     } //end update
 
     public void draw( Graphics2D graphics2 ){ 
+
+        //adds all the letter images into an array in order
+        BufferedImage[] images = {letter_a, letter_b, letter_c, letter_d, letter_e, letter_f, letter_g, letter_h, letter_i, letter_j, letter_k, letter_l, letter_m, letter_n, letter_o, letter_p, letter_q, letter_r, letter_s, letter_t, letter_u, letter_v, letter_w, letter_x, letter_y, letter_z};
         
+        // Only Works with lowercase letters!
+        try {
+            //The txt file has one word per line
+            BufferedReader br = new BufferedReader ( new FileReader("mediumWords.txt") );
+            String line = br.readLine() ;
+            int i = 1; //keeps letters from overlapping
+
+        while(line != null) {
+            // create an array of ascii codes coresponding to each letter in the word
+            byte[] bytes = line.getBytes("US-ASCII");
+            for(byte letter : bytes) {
+
+                //prints each letter corresponding to the ascii code. 
+                //array index is letter - 97 because ascii code starting from lowercase a=97, b=98, c=99 ..etc
+                graphics2.drawImage(images[letter - 97], ( wordsX_2 = wordsX_1 + ( i * gp.tileSize + ( gp.tileSize / 4 ) ) ), wordsY, gp.tileSize, gp.tileSize, null );
+                i++;
+            }//end for
+
+            //prints space here
+            graphics2.drawImage(space, ( wordsX_2 = wordsX_1 + ( i * gp.tileSize + ( gp.tileSize / 4 ) ) ), wordsY, gp.tileSize, gp.tileSize, null );
+            i++;
+            line = br.readLine();
+        }//end while
+
+        br.close();
+        } catch ( IOException e) {
+            e.printStackTrace();
+        }//end catch
+
+ 
+
+/*
         graphics2.drawImage( letter_a, wordsX_1, wordsY, gp.tileSize, gp.tileSize, null ); 
         graphics2.drawImage( letter_b, ( wordsX_2 = wordsX_1 + ( gp.tileSize + ( gp.tileSize / 4 ) ) ), wordsY, gp.tileSize, gp.tileSize, null ); 
         graphics2.drawImage( letter_c, ( wordsX_2 = wordsX_1 + ( 2 * ( gp.tileSize + ( gp.tileSize / 4 ) ) ) ), wordsY, gp.tileSize, gp.tileSize, null );
@@ -91,5 +129,6 @@ public class Words extends Entity {
         graphics2.drawImage( letter_x, ( wordsX_2 = wordsX_1 + ( 23 * ( gp.tileSize + ( gp.tileSize / 4 ) ) ) ), wordsY, gp.tileSize, gp.tileSize, null );
         graphics2.drawImage( letter_y, ( wordsX_2 = wordsX_1 + ( 24 * ( gp.tileSize + ( gp.tileSize / 4 ) ) ) ), wordsY, gp.tileSize, gp.tileSize, null );
         graphics2.drawImage( letter_z, ( wordsX_2 = wordsX_1 + ( 25 * ( gp.tileSize + ( gp.tileSize / 4 ) ) ) ), wordsY, gp.tileSize, gp.tileSize, null );
+*/
     } //end draw
 } //end class
