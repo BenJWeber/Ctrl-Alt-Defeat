6b3d7b94207ec49c029244ebb628360bcc137f56
Remington Crichton <remirc@nmsu.edu>
2022-04-10 16:21:35 -0600

    Fixed reset, added tumbleweed

---
+++ GUI_attempt/src/entity/TumbleWeed.java
@@ -0,0 +1,68 @@
+package GUI_attempt.src.entity;
+
+import GUI_attempt.src.main.GamePanel;
+
+import java.io.IOException;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage; 
+
+import javax.imageio.ImageIO;
+public class TumbleWeed extends Entity {
+    GamePanel gp;
+
+    public TumbleWeed( GamePanel gp ){ 
+        this.gp = gp; 
+         
+        setDefaultValues(); 
+        getImage(); 
+    } //end Player
+
+    public void setDefaultValues(){ 
+        tumbleX = 1500; 
+        tumbleY = 530;
+    } //end setDefaultValues
+
+    public void getImage(){ 
+        try{ 
+            tumble1 = ImageIO.read( getClass().getResourceAsStream( "../tumbleWeed/tumbleWeed_1.png" ) ); 
+            tumble2 = ImageIO.read( getClass().getResourceAsStream( "../tumbleWeed/tumbleWeed_2.png" ) ); 
+            tumble3 = ImageIO.read( getClass().getResourceAsStream( "../tumbleWeed/tumbleWeed_3.png" ) ); 
+            tumble4 = ImageIO.read( getClass().getResourceAsStream( "../tumbleWeed/tumbleWeed_4.png" ) ); 
+
+        } catch( IOException e ){ 
+            e.printStackTrace(); 
+        } //end catch
+    } //end getPlayerImage
+
+    public void update(){ 
+        tumbleX -= speed + 2; 
+        if (spriteCounter > 10 ){ 
+            if( spriteNum == 1 )
+                spriteNum = 2; 
+            else if( spriteNum == 2 )
+                spriteNum = 3;
+            else if( spriteNum == 3 )
+                spriteNum = 4; 
+            else if( spriteNum == 4)
+                spriteNum = 1; 
+            spriteCounter = 0; 
+        } //end if
+       
+        spriteCounter++; 
+    } //end update
+
+    public void draw( Graphics2D graphics2 ){ 
+        BufferedImage image = null; 
+        
+        if( spriteNum == 1 ) 
+            image = tumble1; 
+        if( spriteNum == 2 )
+            image = tumble2;
+        if( spriteNum == 3 )
+            image = tumble3; 
+        if( spriteNum == 4 )
+            image = tumble4; 
+
+        graphics2.drawImage( image, tumbleX, tumbleY, gp.tileSize, gp.tileSize, null ); 
+    } //end draw
+}
